5df5357de7ad4ff7db1f1a56fa78c96deaf0346a
Esther Quiroz <equiroz1811@hotmail.com>
2020-04-27 19:48:27 +0200

    api favoritos

--- ApiRest/apiRest.js
+++ ApiRest/apiRest.js
@@ -20,18 +20,64 @@ connection.connect(function(error){
     if(error){
        console.log(error);
     }else{
-       console.log('Conexion correcta.');
+       console.log('conexion correcta');
     }
  });
 
+ //----------------------------------- API para favoritos -----------------------------------//
 
 
+app.get("/favoritos/:usuario_id", function(req, res, next)
+{ let fav=new Array(''+req.params.usuario_id+'')
+    connection.query("SELECT articulo.articulo_id, articulo.nombre, articulo.imagen, articulo.descripcion, usuario.nick, usuario.lugar, usuario.valoraciones FROM favoritos JOIN usuario ON (favoritos.usuario_id = usuario.usuario_id) JOIN articulo ON (favoritos.articulo_id = articulo.articulo_id) WHERE favoritos.usuario_id=?",fav, function(err, result)
+        {
+            if(err){
+                console.log(err);
+            }else{
+                res.send(result);
+                console.log("GET de favoritos");
+            }
+        }
+    );
+}
+);
+
+app.post("/favoritos", function(req, res, next)
+    {
+        let var1 = "INSERT INTO favoritos (usuario_id, articulo_id) VALUES (?,?)";
+        let var2 = [req.body.usuario_id, req.body.articulo_id];
+
+        connection.query(var1, var2, function(err, result)
+            {
+                if(err){
+                    console.log(err);
+                }else{
+                    res.send(result);
+                    console.log("POST de favoritos");
+                }
+            }
+        );
+    }
+);
+
+app.delete("/favoritos", function(req, res, next)
+    {
+        let nuevo=new Array(''+req.body.favoritos_id+'')
+        let borrado = "DELETE FROM favoritos WHERE favoritos_id = ?"
+
+        connection.query(variable,fav, function(err, result)
+            {
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log(nuevo)
+                    res.send(result);
+                    console.log("DELETE de favoritos");
+                }
+            }
+        );
+    }
+);
 
 
-
-
-
-
-
-
- app.listen(3000)
+app.listen(3000)
